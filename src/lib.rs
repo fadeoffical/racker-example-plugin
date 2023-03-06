03eafe93321bdf7c5d1b9e3d6898a08a9bfcb7de
fade <truefadeoffical@gmail.com>
2023-03-06 02:13:27 +0100

    Update to latest racker version

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-use racker_plugin::{PluginMeta, racker_plugin};
-use racker_plugin::Plugin;
+use racker::plugin::Plugin;
+use racker::plugin::PluginMeta;
+use racker::plugin::racker_plugin;
 
 racker_plugin!(ExamplePlugin, ExamplePlugin::new);
 
