1a177c48aeff71decd08a248880b9b2a00647038
fade <truefadeoffical@gmail.com>
2023-03-06 23:33:50 +0100

    Make cargo use git repo for racker dependency

--- src/lib.rs
+++ src/lib.rs
@@ -13,15 +13,6 @@ impl ExamplePlugin {
 }
 
 impl Plugin for ExamplePlugin {
-    fn meta(&self) -> PluginMeta {
-        PluginMeta {
-            name: String::from("racker-example-plugin"),
-            version: String::from("0.1.0"),
-            description: String::from("An example racker plugin"),
-            author: String::from("fade"),
-        }
-    }
-
     fn on_load(&self) {
         log::info!("Example plugin loaded");
     }
